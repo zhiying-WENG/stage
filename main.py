6889ef1f5904b72890e99fd1265ed763d672ca7a
Gijoe6688 <gijoe6688@gmail.com>
2021-05-31 00:33:59 +0200

    update31052021

--- main.py
+++ main.py
@@ -15,12 +15,34 @@ def upload_file():
   if request.method == 'POST':
     try:
       f = request.files['file']
+      # c'est le excel pour PMU ou hors PMU
+      pmu=request.form['pmu']
+      # occuper les nom de colonns correspend
       cam=request.form['cam']
       lead=request.form['lead']
       cout=request.form['cout']
       clics=request.form['clics']
       impr=request.form["impr"]
       pi=request.form['pi']
+      # ajouter les nouvelle colonnes
+      n1=request.form['n1']
+      n2=request.form['n2']
+      n3=request.form['n3']
+      n4=request.form['n4']
+      n5=request.form['n5']
+      n6=request.form['n6']
+      # les façons de calculer pour les nouvelle colonnes
+      agg1=request.form['agg1']
+      agg2=request.form['agg2']
+      agg3=request.form['agg3']
+      agg4=request.form['agg4']
+      agg5=request.form['agg5']
+      agg6=request.form['agg6']
+      # occuper le nom de nouvelle colonne pour groupby
+      gs=request.form['gs']
+      # le ordre pour groupby
+      ordre=request.form['group']
+      # les elements pour Univers
       u1=request.form['u1']
       u2=request.form['u2']
       u3=request.form['u3']
@@ -31,20 +53,21 @@ def upload_file():
       u8=request.form['u8']
       u9=request.form['u9']
       u10=request.form['u10']
+      # save excel
       tempfile_path=tempfile.NamedTemporaryFile().name
       f.save(tempfile_path)
     except Exception as err:
       return render_template('home.html',err=err)
+      
     # Je charge mon df
     try:
       df = load_excel(tempfile_path)
     except Exception as e:
       return render_template('home.html',e=e)
 
-
     # Je nettoyer df 
     try:
-      response=macro_net(df,cam,lead,cout,clics,impr,pi,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10)
+      response=macro_net(df,cam,lead,cout,clics,impr,pi,pmu,n1,n2,n3,n4,n5,n6,agg1,agg2,agg3,agg4,agg5,agg6,gs,ordre,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10)
     except Exception as ex:
       return render_template('error_excel.html',ex=ex)
    
--- tools.py
+++ tools.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-
 import io
 from flask import make_response
 
@@ -21,7 +20,8 @@ def load_excel(path_file):
 
 """
 
-def macro_net(df,cam,lead,cout,clics,impr,pi,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10):
+def macro_net(df,cam,lead,cout,clics,impr,pi,pmu,n1,n2,n3,n4,n5,n6,agg1,agg2,agg3,agg4,agg5,agg6,gs,ordre,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10):
+    # initialiser les valeurs par defaut si utilisateur n'est pas input
     if cam=="":
         cam="Campaign"
     if lead=="":
@@ -54,12 +54,47 @@ def macro_net(df,cam,lead,cout,clics,impr,pi,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10):
         u9="Autres"
     if u10=="":
         u10="Autres"
-   
-    ls=[cam,lead,cout,clics,impr,pi]
+    # liste de colonnes on veut garder
+    ls=[cam,lead,cout,clics,impr,pi]  
+    # liste de colonnes pour groupby 
+    lgroupby=["Univers"]
+    if gs!="" and gs!=cam:
+        ls+=[gs]
+        if ordre=="nc":
+            lgroupby=[gs]
+        elif ordre=="unc":
+            lgroupby+=[gs]
+        elif ordre=="ncu":
+            lgroupby=[gs]+lgroupby
+    # liste de nouvelle colonnes ajouter
+    ladd=[]
+    # dictionary des nom de colonnes avec ses façon de calculer
+    dictgroupby={"Coût":"sum","Lead":"sum","Clics":"sum","impression":"sum","Requêtes":"sum"}
+    lnumeric=[np.float,np.float16,np.float32,np.float64,np.int,np.int16,np.int32,np.int64]
+    if (n1!="") and (n1 not in ls) and (df[n1].dtype in lnumeric):
+        ladd.append(n1)
+        dictgroupby[n1]=agg1
+    if (n2!="") and (n2 not in ls) and (df[n2].dtype in lnumeric):
+        ladd.append(n2)
+        dictgroupby[n2]=agg2
+    if (n3!="") and (n3 not in ls) and (df[n3].dtype in lnumeric):
+        ladd.append(n3)
+        dictgroupby[n3]=agg3
+    if (n4!="") and (n4 not in ls) and (df[n4].dtype in lnumeric):
+        ladd.append(n4)
+        dictgroupby[n4]=agg4
+    if (n5!="") and (n2 not in ls) and (df[n5].dtype in lnumeric):
+        ladd.append(n5)
+        dictgroupby[n5]=agg5
+    if (n6!="") and (n6 not in ls) and (df[n6].dtype in lnumeric):
+        ladd.append(n6)
+        dictgroupby[n6]=agg6
+    ls+=ladd
+    # garder que cost>0 et les colonnes dans le liste de colonnes on veut garder
     df=df[df.Cost>0].reset_index(drop=True)
     df=df[ls]
-   
-    def Univers(x):
+    # fonction pour traiter univers special pour pmu
+    def UniversPMU(x):
         if "_H_" in x:
             if "_ACQ_" in x:
                 x="TURF ACQ"
@@ -80,6 +115,7 @@ def macro_net(df,cam,lead,cout,clics,impr,pi,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10):
         else:
             x=""
         return x
+    # fonction pour traiter univers general (hors pmu)
     def UniversGen(x):
         if u1 in x:
             x=u1
@@ -104,18 +140,19 @@ def macro_net(df,cam,lead,cout,clics,impr,pi,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10):
         else:
             x="Autres"
         return x
-    if cam=="Campaign" and lead=="Compte API":
-        df["Univers"]=df.Campaign.apply(Univers)
+    if pmu=="ispmu":
+        df["Univers"]=df.Campaign.apply(UniversPMU)
         df=df[~(df.Univers=="TURF")].reset_index(drop=True)
     else:
         df["Univers"]=df[cam].apply(UniversGen)
-
+    # fonction pour traiter search impr share (PI)
     def SearchImpr(x):
         if type(x)==str:
             x=x.replace("<","").replace(">","").replace("%","")
             x=float(x)/100
         return x
     df[pi]=df[pi].apply(SearchImpr)
+
     # df["Requêtes"]=round(df[impr]/df[pi],2) quand il n'y a pas de nan
     # quand pi=nan impression/nan compte directement impression
     l_r=[]
@@ -125,19 +162,23 @@ def macro_net(df,cam,lead,cout,clics,impr,pi,u1,u2,u3,u4,u5,u6,u7,u8,u9,u10):
         else:
             l_r.append(df[impr][i])
     df["Requêtes"]=l_r
-    
+    # renomer les colonnes
     df.rename(columns={lead: "Lead",cout:"Coût",clics:"Clics",impr:"impression",pi:"PI estimé"},inplace=True)
     
-    dfgu=df.groupby("Univers")[["Coût","Lead","Clics","impression","Requêtes"]].sum()
-    
+    # liste de colonnes final dans le excel
+    lfinal=["Requêtes","PI estimé","impression","Clics","CTR estimé","CPC estimé","Coût","TTR estimé","Lead","CPA"]+ladd
+
+    # les liste de  colonnes on veut garder quand groupby:  ["Coût","Lead","Clics","impression","Requêtes"]+ladd
+    # on groupby et calculer
+    dfgu=df.groupby(lgroupby).agg(dictgroupby)
     dfgu["PI estimé"]=round(dfgu.impression/dfgu["Requêtes"],2)
     dfgu["CPA"]=round(dfgu.Coût/dfgu.Lead,2)
     dfgu["CPC estimé"]=round(dfgu.Coût/dfgu.Clics ,2)
     dfgu["CTR estimé"]=round(dfgu.Clics/dfgu.impression ,2)
     dfgu["TTR estimé"]=round(dfgu.Lead/dfgu.Clics ,4)
-    dfgu=dfgu[["Requêtes","PI estimé","impression","Clics","CTR estimé","CPC estimé","Coût","TTR estimé","Lead","CPA"]]
+    dfgu=dfgu[lfinal]
     
-
+    # excel avec les donnes déjà nettoyer
     out = io.BytesIO()
     writer = pd.ExcelWriter(out, engine='xlsxwriter')
     dfgu.to_excel(excel_writer=writer, index=True, sheet_name='Data Search')
